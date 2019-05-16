772812ebefe98f54ef5bb4e45092b5b243e7d34a
Rachele Suy <rachelesuy@gmail.com>
2019-05-16 15:57:51 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,25 +1,23 @@
 class Post 
   attr_accessor :title, :author 
   
-  def initialize(title,   author=nil)
+  @@all = []
   
+  def initialize(title,   author=nil)
     @title = title
     @author = author
-  
     @@all << self
-  
   end
   
   def self.all
    @@all
-    
   end
+  
    def author_name
      if self.author 
        self.author.name 
      else
        nil 
-       
     end
   end
  
