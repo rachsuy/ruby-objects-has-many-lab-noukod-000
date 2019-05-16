475f27210af1b588026d2b0c496a9793521c1b19
Rachele Suy <rachelesuy@gmail.com>
2019-05-16 15:24:22 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -13,4 +13,15 @@ class Author
   def posts
     @posts
   end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    self.post << post
+    post.author = self
+    @@post_count +=1
+  end
+  
+  def self.post_count
+    @@post_count
+  end
 end
